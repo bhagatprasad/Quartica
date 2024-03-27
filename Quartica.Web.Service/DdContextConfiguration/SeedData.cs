0d141c93399ad599af67775fcc7540416eb39f10
Durga Prasad Baghuthu <dbaghuthu@betalen.in>
2024-03-28 05:22:37 +0530

    Add project files.

---
+++ Quartica.Web.Service/DdContextConfiguration/SeedData.cs
@@ -0,0 +1,10 @@
+﻿namespace Quartica.Web.Service.DdContextConfiguration
+{
+    public static class SeedData
+    {
+        public static void Initialize(ApplicationDBContext context)
+        {
+            context.SaveChanges();
+        }
+    }
+}
